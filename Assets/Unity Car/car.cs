72f9bad0c3a1d8c7969303450974443206b68631
TeunisBakker <teunisbakker78@gmail.com>
2025-03-20 09:30:02 +0100

    Feat: removed unused features

--- Assets/Unity Car/car.cs
+++ Assets/Unity Car/car.cs
@@ -1,31 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class car : MonoBehaviour
 {
     public Rigidbody rigid;
-    public WheelCollider wheel1, wheel2, wheel3, wheel4;
+    public wheels[] wheels;
     public float drivespeed, steerspeed;
-    float horizontalInput, verticalInput;
-
-    void Update()
-    {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-    }
 
     void FixedUpdate()
     {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var verticalInput = Input.GetAxis("Vertical");
+
         Rigidbody rb = GetComponent<Rigidbody>();
         rb.centerOfMass = new Vector3(0, -1, 0);
 
-        float motor = Input.GetAxis("Vertical") * drivespeed;
-        wheel1.motorTorque = motor;
-        wheel2.motorTorque = motor;
-        wheel3.motorTorque = motor;
-        wheel4.motorTorque = motor;
-        wheel1.steerAngle = steerspeed * horizontalInput;
-        wheel2.steerAngle = steerspeed * horizontalInput;
+        float motor = verticalInput * drivespeed;
+        for (int i = 0; i < wheels.Length; i++)
+        {
+            var wheelCollider = wheels[i].wheelCollider;
+            wheelCollider.motorTorque = motor;
+
+            if (wheels[i].wheelTurn == true)
+            {
+                wheelCollider.steerAngle = steerspeed * -horizontalInput;
+            }
+        }
     }
 }
